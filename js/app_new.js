fc04feebd874e4f19165090abcd1e2a1a21131f8
oilandsugar <tommy_pm@hotmail.com>
2015-12-21 17:22:01 -0500

    add existing project files

---
+++ js/app_new.js
@@ -0,0 +1,62 @@
+function loadGrammar(name) {
+	// TODO: select grammar name randomly inside function //
+	var grammar = tracery.createGrammar(grammars[name]);
+	var s = grammar.flatten("#origin#");
+	var div = $("<div/>", {
+		id: "outputcontent",
+		html: s
+	});
+
+	$("#output").animate({
+		opacity: 0
+	}, 700, function() {
+		$("#output").html("");
+		$("#output").append(div).animate({
+			opacity: 1
+		}, 700, function(){
+			// TODO: if content is too long, do not generate tweet button but only share button //
+			// TODO: generate facebook share button //
+			$('#social iframe').fadeOut(100).remove();
+			var tweetBtn = $('<a></a>')
+					.addClass('twitter-share-button')
+					.attr('href', 'http://twitter.com/share')
+					.attr('target', '_blank')
+					.attr('data-url', 'http://www.gazeupinto.space/')
+					.attr('data-size', 'large')
+					.attr('data-count', 'none')
+					.attr('data-text', $('#outputcontent').text());
+			$('#social').append(tweetBtn);
+			twttr.widgets.load();
+			$('#social').fadeIn(500);
+		});
+	});
+
+	// TODO: animate stars on quote generation //
+}
+
+$(document).ready(function() {
+	
+	var rand = Math.floor((Math.random() * 10) + 1);
+	if(rand < 4) {
+		$('<img/>').attr('src', 'http://www.gazeupinto.space/img/1.jpg').load(function() {
+			$(this).remove(); 
+			$('#galaxy').css('background-image', 'url(http://www.gazeupinto.space/img/1.jpg)').animate({opacity: 0.3}, 1000);
+		});
+	} else if(rand > 7) {
+		$('<img/>').attr('src', 'http://www.gazeupinto.space/img/2.jpg').load(function() {
+			$(this).remove(); 
+			$('#galaxy').css('background-image', 'url(http://www.gazeupinto.space/img/2.jpg)').animate({opacity: 0.3}, 1000);
+		});
+	} else {
+		$('<img/>').attr('src', 'http://www.gazeupinto.space/img/3.jpg').load(function() {
+			$(this).remove(); 
+			$('#galaxy').css('background-image', 'url(http://www.gazeupinto.space/img/3.jpg)').animate({opacity: 0.3}, 1000);
+		});
+	}
+		
+	setTimeout(function() {
+		loadGrammar("universe");
+		$('#buttons').delay(1000).fadeIn(500);
+	}, 0);
+
+});
